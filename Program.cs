cae00c363b7d5b33f30c4de5ba473fc3830aa8d0
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-30 14:22:16 +0530

    [Biplab] Add: Dequeue data

--- Program.cs
+++ Program.cs
@@ -3,15 +3,18 @@
 namespace StackAndQueues
 {
     public class Program
-{
-    public static void Main(String[] args)
     {
-        Console.WriteLine("Welcome to Stack and Queue!!");
-        Queue queue = new Queue();
-        queue.Enqueue(56);
-        queue.Enqueue(30);
-        queue.Enqueue(70);
-        queue.DisplayQueue();
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue!!");
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.DisplayQueue();
+
+            queue.Dequeue();
+            queue.DisplayQueue();
+        }
     }
-}
 }
